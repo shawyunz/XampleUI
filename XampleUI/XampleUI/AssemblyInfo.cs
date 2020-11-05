fec3eebb58f4a2fb07b848b90efae95cdfe687cb
shawyunz <shawyunz@gmail.com>
2020-11-05 18:56:49 +1300

    :bug: remove custom font info from repo. #Cake

--- XampleUI/XampleUI/AssemblyInfo.cs
+++ XampleUI/XampleUI/AssemblyInfo.cs
@@ -3,5 +3,3 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: ExportFont("Gilroy-ExtraBold.otf", Alias = "GilroyEB")]
-[assembly: ExportFont("Gilroy-Light.otf", Alias = "GilroyL")]
