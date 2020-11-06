e17315dd5437e386762da6dc5c4131072e424c54
shawyunz <shawyunz@gmail.com>
2020-11-07 01:06:36 +1300

    :sparkles: Uneven list view UI updated.
    :poop: UI in C# needs review.
    One cart image issue left.
    Transition to detail left.

--- XampleUI/XampleUI/Views/DribGrocs/GrocList.xaml.cs
+++ XampleUI/XampleUI/Views/DribGrocs/GrocList.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XampleUI.Models;
 using XampleUI.ViewModels.Groc;
 
 namespace XampleUI.Views.DribGrocs
@@ -8,6 +10,7 @@ namespace XampleUI.Views.DribGrocs
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class GrocList : ContentPage
 	{
+		private int _index;
 		private GrocsViewModel _viewModel;
 
 		public GrocList()
@@ -21,6 +24,51 @@ namespace XampleUI.Views.DribGrocs
 		{
 			base.OnAppearing();
 			_viewModel.OnAppearing();
+
+			var list = _viewModel.Grocs;
+			if (list is null)
+			{
+				return;
+			}
+
+			var box = new BoxView { CornerRadius = 10, HeightRequest = 56 };
+			RightLane.Children.Add(box);
+
+			foreach (var item in list)
+			{
+				AddItemLayout(item, _index);
+				_index++;
+			}
+		}
+
+		private void AddItemLayout(Item item, int index)
+		{
+			int _baseIndex = index / 2;
+			var _flagRight = Convert.ToBoolean(index % 2);
+
+			var lane = _flagRight ? RightLane : LeftLane;
+
+			var frame = new Frame { HeightRequest = 272, Margin = 8, Padding = 0, CornerRadius = 10 };
+			var stack = new StackLayout { Padding = 22, BackgroundColor = Color.White };
+
+			var image = new Image { Source = item.Image, HeightRequest = 88, HorizontalOptions = LayoutOptions.Center, Margin = new Thickness(0, 16) };
+			var labelPrice = new Label { Margin = new Thickness(0, 10), FontAttributes = FontAttributes.Bold, FontSize = 22, Text = "$" + item.Price, TextColor = Color.FromHex("#333333") };
+			var labelText  = new Label { FontAttributes = FontAttributes.Bold, FontSize = 14, Text = item.Text, TextColor = Color.FromHex("#333333") };
+			var labelSize  = new Label { LineBreakMode = LineBreakMode.NoWrap, FontSize = 13, Text = item.Size, TextColor = Color.FromHex("#CCCCCC") };
+
+			stack.Children.Add(image);
+			stack.Children.Add(labelPrice);
+			stack.Children.Add(labelText);
+			stack.Children.Add(labelSize);
+			frame.Content = stack;
+			lane.Children.Add(frame);
+
+			var tap = new TapGestureRecognizer();
+			tap.Tapped += async (object sender, EventArgs e) =>
+			{
+				await Shell.Current.GoToAsync($"{nameof(GrocDetail)}?{nameof(GrocDetailViewModel.ItemId)}={item.Id}");
+			};
+			stack.GestureRecognizers.Add(tap);
 		}
 
 		private async void SwipeToShowCart(object sender, SwipedEventArgs e)
